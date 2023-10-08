0bacc91d1994efeb7419945fe1d4c5a0b53cbac4
Prashanth Kumar Kadasi <75978417+kprsnt2@users.noreply.github.com>
2023-10-09 02:13:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ formula_input = st.text_input("Enter a chemical formula (e.g., H2O):")
 
 if formula_input:
     try:
-        # Search for compounds with the given formula on PubChem
+        # Try searching for compounds with the given formula using the 'formula' query type
         compounds = pcp.get_compounds(formula_input, 'formula')
 
         if compounds:
@@ -27,7 +27,19 @@ if formula_input:
             st.write(f"- Synonyms: {', '.join(compound.synonyms)}")
 
         else:
-            st.warning("No information found on PubChem for this formula.")
+            # If no compounds are found, retry with a different query type ('name')
+            name_compounds = pcp.get_compounds(formula_input, 'name')
+
+            if name_compounds:
+                compound = name_compounds[0]  # Take the first search result
+                st.subheader("Basic Information:")
+                st.write(f"- Formula: {compound.molecular_formula}")
+                st.write(f"- Molecular Weight: {compound.molecular_weight:.2f} g/mol")
+                st.subheader("Additional Information:")
+                st.write(f"- IUPAC Name: {compound.iupac_name}")
+                st.write(f"- Synonyms: {', '.join(compound.synonyms)}")
+            else:
+                st.warning("No information found on PubChem for this formula.")
 
     except Exception as e:
         st.error(f"Error: {str(e)}")
