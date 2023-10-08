061ac0c008db5a6d0b7514025e033e3285c5a5dd
Prashanth Kumar Kadasi <75978417+kprsnt2@users.noreply.github.com>
2023-10-09 02:18:50 +0530

    library change

--- app.py
+++ app.py
@@ -1,48 +1,42 @@
 import streamlit as st
-import pubchempy as pcp
+from chempy import Substance
 
-# Streamlit app title and description
+
+# Streamlit app title
 st.title("Chemical Formula Info Lookup")
-st.write("Enter a chemical formula to learn more about it!")
 
 # User input for chemical formula
 formula_input = st.text_input("Enter a chemical formula (e.g., H2O):")
 
 if formula_input:
     try:
-        # Try searching for compounds with the given formula using the 'formula' query type
-        compounds = pcp.get_compounds(formula_input, 'formula')
+        # Create a Substance object from the user's input
+        substance = Substance.from_formula(formula_input)
 
-        if compounds:
-            compound = compounds[0]  # Take the first search result
+        # Display basic information about the substance
+        st.write("Formula:", substance.formula)
+        st.write("Molar mass:", substance.mass)
 
-            # Display basic information about the compound
-            st.subheader("Basic Information:")
-            st.write(f"- Formula: {compound.molecular_formula}")
-            st.write(f"- Molecular Weight: {compound.molecular_weight:.2f} g/mol")
+        # Search for more information about the substance on PubChem
+        search_results = PubChem.search(substance)
 
-            # Display more information (you can add more details as needed)
-            st.subheader("Additional Information:")
-            st.write(f"- IUPAC Name: {compound.iupac_name}")
-            st.write(f"- Synonyms: {', '.join(compound.synonyms)}")
+        if search_results:
+            result = search_results[0]  # Take the first search result
+            st.write("\nAdditional information from PubChem:")
+            st.write("Common name:", result.common_name)
+            st.write("IUPAC name:", result.iupac_name)
+            st.write("Molecular weight:", result.molecular_weight)
+            st.write("CAS Registry Number:", result.cas)
 
         else:
-            # If no compounds are found, retry with a different query type ('name')
-            name_compounds = pcp.get_compounds(formula_input, 'name')
-
-            if name_compounds:
-                compound = name_compounds[0]  # Take the first search result
-                st.subheader("Basic Information:")
-                st.write(f"- Formula: {compound.molecular_formula}")
-                st.write(f"- Molecular Weight: {compound.molecular_weight:.2f} g/mol")
-                st.subheader("Additional Information:")
-                st.write(f"- IUPAC Name: {compound.iupac_name}")
-                st.write(f"- Synonyms: {', '.join(compound.synonyms)}")
-            else:
-                st.warning("No information found on PubChem for this formula.")
+            st.warning("No information found on PubChem for this formula.")
 
     except Exception as e:
         st.error(f"Error: {str(e)}")
 
-    # Add a "Reset" button to clear the input field
-    st.button("Reset")
+# Add a link to PubChem for more details
+st.markdown("[More details on PubChem](https://pubchem.ncbi.nlm.nih.gov/)")
+
+# This line allows the Streamlit app to run in the browser
+if __name__ == "__main__":
+    st.set_page_config(page_title="Chemical Formula Info Lookup")
